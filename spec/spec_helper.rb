e26aabcb16bb908c853b3a013e8bd4c65d7bbbc2
zhaqiang <hugochougt@gmail.com>
2013-12-26 22:27:36 +0800

    Add simple home, help and about pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Add Capybara DSL into RSpec help file
+  config.include Capybara::DSL
 end
