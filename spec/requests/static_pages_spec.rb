94393e0baf48240ee61bc98019bda6ca50d05de3
zhaqiang <hugochougt@gmail.com>
2013-12-28 20:37:47 +0800

    Exercise 5.6.2: Use shared example to refactor static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,34 +4,46 @@ describe "StaticPages" do
 
   subject { page }
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  shared_examples_for "all static pages" do
+    it { should have_title(full_title(page_title)) }
+    it { should have_content(heading) }
+  end
 
   describe "Home page" do
     before { visit root_path  }
 
-    it { should have_content('Sample App') }
-    it { should have_title("#{base_title}") }
+    let(:heading) { 'Sample App'}
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:page_title) { 'Help' }
+    let(:heading) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_title(full_title('About Us')) }
-    it { should have_content('About') }
+    let(:page_title) { 'About' }
+    let(:heading) { 'About' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_title(full_title('Contact')) }
+    let(:page_title) { 'Contact' }
+    let(:heading) { 'Contact' }
+
+    it_should_behave_like "all static pages"
     it { should have_selector('h1', text: 'Contact') }
   end
 
