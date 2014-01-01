4979302a2bd32f07e39d250a40d7c5d8d9c3fdf4
zhaqiang <hugochougt@gmail.com>
2014-01-01 16:21:41 +0800

    Fix typo

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,7 +25,7 @@ describe "UserPages" do
         expect { click_button submit }.not_to change(User, :count)
       end
 
-      it "after submission" do
+      describe "after submission" do
         before { click_button submit }
 
         it { should have_title("Sign up") }
